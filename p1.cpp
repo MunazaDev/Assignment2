162c166630275f4aee507df8cd0888622f0aff98
Munaza Rauf <munazarauf31@gmail.com>
2026-03-01 10:14:18 -0800

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main () {
+cout<<"Enter your Name";
+string Name;
+cin>> Name;
+cout<<"Enter your Roll No";
+int RollNo;
+cin>> RollNo;
+cout<<"Enter your Aggregate";
+int Aggregate;
+cin>> Aggregate;
+cout<<"Enter your Section";
+char Section;
+cin>> Section;
+}
