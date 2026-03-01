873b83fa61bcad21f4a0d57cebe01f4c76bc2186
Munaza Rauf <munazarauf31@gmail.com>
2026-03-01 10:16:29 -0800

    Add files via upload

---
+++ p4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main () {
+cout<<"Enter the charge";
+int charge;
+cin>> charge;
+cout<<"Enter the time";
+int time;
+cin>>time;
+int current;
+current=charge/time;
+cout<<"The current is"<<current;
+
+}
+
