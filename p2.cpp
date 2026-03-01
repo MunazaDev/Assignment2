b0830e8acb4aec071075403d046b7a1f55a884ab
Munaza Rauf <munazarauf31@gmail.com>
2026-03-01 10:15:40 -0800

    Add files via upload

---
+++ p2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main () {
+cout<<"Enter the weight in lbs";
+int Weightinlbs;
+cin>> Weightinlbs;
+int Weightinkgs;
+Weightinkgs = Weightinlbs*0.45;
+cout<<"TheWeightinkgswas"<<Weightinkgs;
+
+}
+
