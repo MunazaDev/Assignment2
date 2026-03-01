7f54195897a1a3e057d1cb4da438d9e2c74aa378
Munaza Rauf <munazarauf31@gmail.com>
2026-03-01 10:16:05 -0800

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main () {
+cout<<"Enter the length";
+int length;
+cin>>length;
+cout<<"Enter the width";
+int width;
+cin>>width;
+int Area;
+Area=length*width;
+cout<<"The Area is"<<Area;
+
+
+}
+
